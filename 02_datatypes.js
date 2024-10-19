5f07806256561246fb4f096029e828637b792334
Dark0boi <88085762+SnehitPandey@users.noreply.github.com>
2024-10-20 00:16:03 +0530

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code with newer version. not required to write as of newer versions run on new ..
+
+//alert(3+3) // we are using nodejs not browser 
+
+console.log( 3 
+    + 3 ); // not a good practice code readability is important
+
+console.log("Snehit");
+
+//Datatypes
+
+let name = "Snehit" // String datatype
+let age = 20        // int 
+let islogged = true // true or false // boolean   
+//let state = null  //standalone 
+let state;          // undefined
+    
+//number => 2 to power 53
+//bigint = for very big no.s
+//string = "abc"
+//boolean = true / false
+//null = standalone value 
+//unidentified = value is not assigned
+//symbol = 
+
